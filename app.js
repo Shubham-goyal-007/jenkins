aced4dba5e8efc4d84d3c5cc9e6f580eb4bdfc4c
Shubham-goyal-007 <105415770+Shubham-goyal-007@users.noreply.github.com>
2023-05-12 15:40:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello Shubham Welcome to AWS !'))
+app.get('/', (req, res) => res.send('Hello Shubham Goyal Welcome to AWS !'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
